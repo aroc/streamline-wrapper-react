b17e38a4f4cb65a11e73ceb33182f3c8e31dc986
Clément SAUVAGE <hello@clementsauvage.me>
2020-04-21 09:55:08 +0200

    Add test structure

---
+++ packages/streamline-icons-react/src/components/__tests__/StreamlineIcon.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import StreamlineIcon from "../StreamlineIcon";
+import {TouchId} from '../../../'
+
+const touchIdLock =
+// Just some dummy test
+
+describe('My Test Suite', () => {
+  it('My Test Case', () => {
+    expect(true).toEqual(true);
+  });
+});
+
+
+describe('Render a component', () => {
+  it('My Test Case', () => {
+    expect(true).toEqual(true);
+  });
+});
